cd30484582bc8c8c7ab0e2bdde872d0b1fa22182
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-21 14:57:13 -0600

    Add tests for ContentButton component iteration 1

--- src/ContentButton/index.test.js
+++ src/ContentButton/index.test.js
@@ -3,9 +3,26 @@ import { shallow, mount } from 'enzyme';
 import ContentButton from './index.js';
 
 describe('ContentButton', () => {
+  let wrapper;
+  let mockButtonType;
+  let mockActivateCategory;
+
+  beforeEach( () => {
+    mockButtonType = 'people';
+    mockActivateCategory = jest.fn();
+    wrapper = shallow(
+      <ContentButton 
+        buttonType={mockButtonType}
+        activateCategory={mockActivateCategory}/>);
+  })
 
   it('should match the snapshot', () => {
-    const wrapper = shallow(<ContentButton />);
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should run activateCategory with correct params on click', () =>{
+    const expected = mockButtonType;
+    wrapper.find('.content-button').simulate('click');
+    expect(mockActivateCategory).toHaveBeenCalledWith(expected)
   })
 })
