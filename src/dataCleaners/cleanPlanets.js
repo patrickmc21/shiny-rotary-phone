6fb31ec2992e00449cf053cc7224139d44544ee4
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-22 13:41:29 -0600

    iteration 2 data fetch complete

--- src/dataCleaners/cleanPlanets.js
+++ src/dataCleaners/cleanPlanets.js
@@ -1,5 +1,26 @@
-const cleanPlanets = (planetsDate) => {
-  console.log(planetsDate)
+const fetchResidents = (planetResidents) => {
+  const planets = planetResidents.map(resident => {
+    return fetch(resident)
+      .then(response => response.json())
+      .then(resident => resident.name)
+  })
+  return Promise.all(planets);
+}
+
+const cleanPlanets = (planetsData) => {
+  const planets = planetsData.map( async (planet) => {
+      let residents = await fetchResidents(planet.residents);
+      return (
+        {
+          name: planet.name, 
+          Terrain: planet.terrain, 
+          Population: planet.population, 
+          Climate: planet.climate, 
+          Residents: residents.join(', ')
+        }
+      )
+    });
+  return Promise.all(planets)
 }
 
 export { cleanPlanets };
--- src/dataCleaners/cleanVehicles.js
+++ src/dataCleaners/cleanVehicles.js
@@ -1,5 +1,15 @@
 const cleanVehicles = (vehicleData) => {
-  console.log(vehicleData);
+  const vehicles = vehicleData.map(vehicle => {
+    return (
+      {
+        name: vehicle.name, 
+        Model: vehicle.model, 
+        Class: vehicle.vehicle_class, 
+        'Number Of Passeneger': vehicle.passengers
+      }
+    )
+  })
+  return Promise.all(vehicles)
 }
 
 export { cleanVehicles };
