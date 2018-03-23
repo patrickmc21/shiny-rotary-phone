5a8df115b2f4843199ca43b7f2645de6693cb088
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-22 20:19:46 -0600

    iteration 2 test and mock data setup

---
+++ src/mockData/cleanedPlanetData.js
@@ -0,0 +1,72 @@
+module.exports = [
+  {
+    name: 'Alderaan',
+    Terrain: 'grasslands, mountains',
+    Population: '2000000000',
+    Climate: 'temperate',
+    Residents: 'Luke Skywalker, C-3PO, R2-D2'
+  },
+  {
+    name: 'Yavin IV',
+    Terrain: 'jungle, rainforests',
+    Population: '1000',
+    Climate: 'temperate, tropical',
+    Residents: ''
+  },
+  {
+    name: 'Hoth',
+    Terrain: 'tundra, ice caves, mountain ranges',
+    Population: 'unknown',
+    Climate: 'frozen',
+    Residents: ''
+  },
+  {
+    name: 'Dagobah',
+    Terrain: 'swamp, jungles',
+    Population: 'unknown',
+    Climate: 'murky',
+    Residents: ''
+  },
+  {
+    name: 'Bespin',
+    Terrain: 'gas giant',
+    Population: '6000000',
+    Climate: 'temperate',
+    Residents: 'Luke Skywalker'
+  },
+  {
+    name: 'Endor',
+    Terrain: 'forests, mountains, lakes',
+    Population: '30000000',
+    Climate: 'temperate',
+    Residents: 'Luke Skywalker'
+  },
+  {
+    name: 'Naboo',
+    Terrain: 'grassy hills, swamps, forests, mountains',
+    Population: '4500000000',
+    Climate: 'temperate',
+    Residents: 'Luke Skywalker, C-3PO, R2-D2, Darth Vader, Leia Organa'
+  },
+  {
+    name: 'Coruscant',
+    Terrain: 'cityscape, mountains',
+    Population: '1000000000000',
+    Climate: 'temperate',
+    Residents: 'Luke Skywalker, C-3PO, R2-D2'
+  },
+  {
+    name: 'Kamino',
+    Terrain: 'ocean',
+    Population: '1000000000',
+    Climate: 'temperate',
+    Residents: 'Luke Skywalker, C-3PO'
+  },
+  {
+    name: 'Geonosis',
+    Terrain: 'rock, desert, mountain, barren',
+    Population: '100000000000',
+    Climate: 'temperate, arid',
+    Residents: 'Luke Skywalker'
+  }
+]
